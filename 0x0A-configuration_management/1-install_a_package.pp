ed2095bbeb4ebf6ebd1f36c41d4f7782db790a3f
abdelghafour99 <abdelghafour83@live.fr>
2024-03-22 10:13:14 -0700

    0x0A-configuration_management commit 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 #!/usr/bin/pup
 # Install a specific version of flask 2.1.0
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
+exec { 'flask':
+  command => '/usr/bin/apt-get -y install flask -v 2.1.0',
 }
