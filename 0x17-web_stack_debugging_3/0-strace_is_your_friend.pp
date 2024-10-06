05b4113875a3a513b94ab48a9767f4cf07b96b02
abdelghafour99 <abdelghafour83@live.fr>
2024-10-06 11:41:29 +0000

    file00

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 #Fixes bad 'phpp' Extensions to 'php' in the WordPress File 'wp-settings.php'
 
 exec { 'fix-wordpress':
- i command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
   path    => '/usr/local/bin/:/bin/'
 }
