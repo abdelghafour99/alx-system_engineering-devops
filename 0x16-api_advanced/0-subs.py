85e215b7078404677eb2a65b3a1201fdac36be1d
abdelghafour99 <abdelghafour83@live.fr>
2024-10-08 10:50:02 -0700

    file 00

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,15 +8,14 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit"""
+    """Return the total number of subscribers on a given subreddit."""
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    head = {"User-Agent": "My-Unique-User-Agent"}
-
-    resp = requests.get(url, headers=head, allow_redirects=False)
-
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    resp = requests.get(url, headers=headers, allow_redirects=False)
     if resp.status_code == 404:
         return 0
-
-    data = resp.json().get("data")
-    return data.get("subscribers")
+    res = resp.json().get("data")
+    return res.get("subscribers")
